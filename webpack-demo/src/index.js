9fa08eec63df2fb86e76455ff7b3b9c024c90f51
daniel-kav <dankavatha01@gmail.com>
2023-08-26 07:44:39 +0300

    configuring webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,7 +1,10 @@
+
+import _ from 'lodash';
+
 function component(){
     const element = document.createElement('div');
 
-    //lodash currently is includedvia a script, is required for this line to work
+    
     element.innerHTML = _join(['Hello' , 'webpack']);
 
     return element;
